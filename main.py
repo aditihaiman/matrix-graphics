95720fa0faa94452680e622e0c6235229f0bd320
aditihaiman <ahaiman00@stuy.edu>
2020-02-16 10:43:18 -0500

    started self portrait

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from display import *
 from draw import *
 from matrix import *
+import math
 
 screen = new_screen()
 color = [ 0, 255, 0 ]
@@ -43,5 +44,47 @@ print("\n")
 matrix_mult(matrix, matrix2)
 print_matrix(matrix2)
 
+matrix = []
+#change color to brown
+x0 = 0
+y0 = 0
+for x in range(100):
+    x0 = 75*math.cos(x)+300
+    y0 = 25*math.sin(x) + 100
+    add_point(matrix, x0, y0, 1)
+    
 draw_lines( matrix, screen, color )
-#display(screen)
+
+color = [0, 0, 255]
+matrix = []
+    
+for x in range(100):
+    x0 = 10*math.cos(x)+250
+    y0 = 200*math.sin(x) + 200
+    add_point(matrix, x0, y0, 1)
+
+draw_lines( matrix, screen, color )
+
+matrix = []
+    
+for x in range(100):
+    x0 = 7*math.cos(x)+350
+    y0 = 60*math.sin(x) + 60
+    add_point(matrix, x0, y0, 1)
+
+draw_lines( matrix, screen, color )
+
+color = [0, 0, 255]
+matrix = []
+    
+for x in range(100):
+    x0 = 10*math.cos(x)+250
+    y0 = 200*math.sin(x) + 200
+    add_point(matrix, x0, y0, 1)
+
+draw_lines( matrix, screen, color )
+
+
+
+
+display(screen)
